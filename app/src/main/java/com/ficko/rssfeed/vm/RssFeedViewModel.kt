642cf313254123554bc48994af2349842f2f3278
Tomislav Ficko <ficko.tomislav@gmail.com>
2022-08-05 21:34:23 +0200

    Integrate repo

--- app/src/main/java/com/ficko/rssfeed/vm/RssFeedViewModel.kt
+++ app/src/main/java/com/ficko/rssfeed/vm/RssFeedViewModel.kt
@@ -1,46 +1,37 @@
 package com.ficko.rssfeed.vm
 
 import androidx.lifecycle.MutableLiveData
+import com.ficko.rssfeed.data.RssFeedRepository
 import com.ficko.rssfeed.domain.RssFeed
 import com.ficko.rssfeed.domain.RssFeedItem
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class RssFeedViewModel @Inject constructor() : BaseViewModel() {
+class RssFeedViewModel @Inject constructor(
+    private val repository: RssFeedRepository
+) : BaseViewModel() {
 
     val getRssFeedsSuccess = MutableLiveData<List<RssFeed>>()
     val getRssFeedItemsSuccess = MutableLiveData<List<RssFeedItem>>()
     val addNewFeedSuccess = MutableLiveData<Unit>()
 
     fun getRssFeeds() {
-        getRssFeedsSuccess.postValue(getSampleFeeds())
-    }
-
-    fun getRssFeedItems(feed: RssFeed) {
-        getRssFeedItemsSuccess.postValue(getSampleFeedItems())
-    }
-
-    fun addNewFeed(url: String) {
-        addNewFeedSuccess.postValue(Unit)
-    }
-
-    private fun getSampleFeeds() = List(6) {
-        RssFeed().apply {
-            name = "Feed"
-            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been..."
-            url = "https://feed.com"
-            imageUrl = "https://picsum.photos/200"
-            items = getSampleFeedItems()
+        executeUseCase {
+            repository.updateRssFeeds()
+            val response = repository.getRssFeeds()
+            getRssFeedsSuccess.postValue(response)
         }
     }
 
-    private fun getSampleFeedItems() = List(6) {
-        RssFeedItem().apply {
-            name = "Item"
-            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been..."
-            url = "https://item.com"
-            imageUrl = "https://picsum.photos/200"
+    fun getRssFeedItems(feed: RssFeed) {
+        getRssFeedItemsSuccess.postValue(feed.items)
+    }
+
+    fun addNewFeed(url: String) {
+        executeUseCase {
+            repository.addNewFeed(url)
+            addNewFeedSuccess.postValue(Unit)
         }
     }
 }
