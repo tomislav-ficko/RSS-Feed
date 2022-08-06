eb4dd57a17d3a90e2803ddabd952a0c2a409c783
Tomislav Ficko <ficko.tomislav@gmail.com>
2022-08-06 11:13:23 +0200

    Fix UI not being displayed by WebViewActivity

--- app/src/main/java/com/ficko/rssfeed/ui/WebViewActivity.kt
+++ app/src/main/java/com/ficko/rssfeed/ui/WebViewActivity.kt
@@ -26,6 +26,7 @@ class WebViewActivity : BaseActivity(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
         setUpActivity()
     }
 
